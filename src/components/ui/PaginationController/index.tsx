b9b9902e4ed668c7bd04908ccb0d741853748527
phong28zk <lamperougelelouchanm@gmail.com>
2024-10-25 09:14:38 +0700

    feat/filter-extension: fix pagination

--- src/components/ui/PaginationController/index.tsx
+++ src/components/ui/PaginationController/index.tsx
@@ -11,8 +11,8 @@ import {
 export interface IPaginationControllerProps {
   limit: number;
   totalFiles: number;
-  currentPage: number; 
-  onPageChange: (page: number) => void; 
+  currentPage: number;
+  onPageChange: (page: number) => void;
 }
 
 export default function PaginationController({
@@ -25,6 +25,7 @@ export default function PaginationController({
   const startFile = (currentPage - 1) * limit + 1;
   const endFile = Math.min(currentPage * limit, totalFiles);
 
+  console.log("Total pages", totalPages);
   return (
     <>
       <div>
@@ -38,20 +39,85 @@ export default function PaginationController({
               onClick={() => currentPage > 1 && onPageChange(currentPage - 1)}
             />
           </PaginationItem>
-          {Array.from({ length: totalPages }, (_, index) => (
-            <PaginationItem key={index}>
-              <PaginationLink
-                href="#"
-                isActive={currentPage === index + 1}
-                onClick={() => onPageChange(index + 1)}
-              >
-                {index + 1}
-              </PaginationLink>
-            </PaginationItem>
-          ))}
-          <PaginationItem>
-            <PaginationEllipsis />
-          </PaginationItem>
+          {totalPages <= 5 ? (
+            Array.from({ length: totalPages }, (_, index) => (
+              <PaginationItem key={index}>
+                <PaginationLink
+                  href="#"
+                  isActive={currentPage === index + 1}
+                  onClick={() => onPageChange(index + 1)}
+                >
+                  {index + 1}
+                </PaginationLink>
+              </PaginationItem>
+            ))
+          ) : (
+            <>
+              {currentPage !== 1 && (
+                <PaginationItem>
+                  <PaginationLink
+                    href="#"
+                    isActive={currentPage === 1}
+                    onClick={() => onPageChange(1)}
+                  >
+                    1
+                  </PaginationLink>
+                </PaginationItem>
+              )}
+              {currentPage > 3 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+              {currentPage > 2 && (
+                <PaginationItem>
+                  <PaginationLink
+                    href="#"
+                    isActive={currentPage === currentPage - 1}
+                    onClick={() => onPageChange(currentPage - 1)}
+                  >
+                    {currentPage - 1}
+                  </PaginationLink>
+                </PaginationItem>
+              )}
+              <PaginationItem>
+                <PaginationLink
+                  href="#"
+                  isActive={true}
+                  onClick={() => onPageChange(currentPage)}
+                >
+                  {currentPage}
+                </PaginationLink>
+              </PaginationItem>
+              {currentPage < totalPages - 1 && (
+                <PaginationItem>
+                  <PaginationLink
+                    href="#"
+                    isActive={currentPage === currentPage + 1}
+                    onClick={() => onPageChange(currentPage + 1)}
+                  >
+                    {currentPage + 1}
+                  </PaginationLink>
+                </PaginationItem>
+              )}
+              {currentPage < totalPages - 2 && (
+                <PaginationItem>
+                  <PaginationEllipsis />
+                </PaginationItem>
+              )}
+                {currentPage !== totalPages && (
+                <PaginationItem>
+                  <PaginationLink
+                  href="#"
+                  isActive={currentPage === totalPages}
+                  onClick={() => onPageChange(totalPages)}
+                  >
+                  {totalPages}
+                  </PaginationLink>
+                </PaginationItem>
+                )}
+            </>
+          )}
           <PaginationItem>
             <PaginationNext
               href="#"
