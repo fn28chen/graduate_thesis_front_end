2a84c9797e32f0028166552304014332b16d2b31
phong28zk <lamperougelelouchanm@gmail.com>
2024-11-06 09:45:50 +0700

    feat/profile: toaster and profile

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,6 +1,10 @@
 "use client";
 import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import { buttonVariants } from "@/components/ui/Button/button";
+import { cn } from "@/lib/utils";
 import { AvatarImage } from "@radix-ui/react-avatar";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
 import React from "react";
 
 interface IUser {
@@ -13,7 +17,6 @@ interface IUser {
 }
 function Profile() {
   const storageUser = localStorage.getItem("user");
-
   const user: IUser | null = storageUser ? JSON.parse(storageUser) : null;
   const userProfile = user?.user;
 
@@ -23,17 +26,33 @@ function Profile() {
 
   return (
     <div className="m-10">
-      <h1 className="text-2xl">Profile</h1>
-      <div className="flex flex-col items-center justify-center w-full">
-        <Avatar>
+      <Link
+        href="/"
+        className={cn(
+          buttonVariants({ variant: "ghost" }),
+          "absolute left-4 top-4 md:left-8 md:top-8"
+        )}
+      >
+        <ArrowLeft className="w-6 h-6" />
+      </Link>
+      <div className="flex flex-col items-center justify-center w-full gap-4">
+        <h1 className="text-2xl">Profile</h1>
+        <Avatar className="w-24 h-24">
           <AvatarImage src={userProfile?.avatar} alt={userProfile?.username} />
-          <AvatarFallback>{userProfile?.username[0]}</AvatarFallback>
+          <AvatarFallback delayMs={200} className="text-3xl">
+            {userProfile?.username[0]}
+          </AvatarFallback>
         </Avatar>
-        <h1>{userProfile?.username}</h1>
+        <h1 className="text-3xl font-bold">{userProfile?.username}</h1>
       </div>
       <div className="mt-5">
         <p>Email: {userProfile?.email}</p>
-        <p>Created At: {userProfile?.createdAt ? new Date(userProfile.createdAt).toLocaleDateString('en-GB') : ''}</p>
+        <p>
+          Created At:{" "}
+          {userProfile?.createdAt
+            ? new Date(userProfile.createdAt).toLocaleDateString("en-GB")
+            : ""}
+        </p>
       </div>
     </div>
   );
