cdbab22591fccac1f4a6c2e55a046618e8430bf2
phong28zk <lamperougelelouchanm@gmail.com>
2024-12-08 12:23:42 +0700

    chore: update config

--- next.config.mjs
+++ next.config.mjs
@@ -21,6 +21,27 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://graduate-thesis-cloud-drive.site",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
